0fe9565a9a7a4de99c05e7ae7a25f17fe3beed5f
cjs7777 <1093844645@qq.com>
2021-05-15 21:45:45 +0800

    完善导入功能并实现保存功能

--- code/Remark.py
+++ code/Remark.py
@@ -2,6 +2,8 @@ import json
 from tkinter import *
 import tkinter.filedialog
 import csv
+
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QInputDialog, QLineEdit, QWidget, QTableWidgetItem, QFileDialog
 
 
@@ -30,24 +32,58 @@ class RemarkModule:
         self.LabelClassDict = {}
         self.getLabel(file_path)
         self.load_label_ComboBox()
+        self.TableInit()
         # print(self.LabelClassDict)
+
+    def TableInit(self):
+
+        ColumnCount = self.LabelClassDict.__len__()+1
+        self.ui.remark_lW_list.setColumnCount(ColumnCount)
+        for i in range(len(self.LabelClassDict)):
+            item = QtWidgets.QTableWidgetItem()
+            item.setText(list(self.LabelClassDict.keys())[i])
+            self.ui.remark_lW_list.setHorizontalHeaderItem(i+1, item)
 
     # 文件选择获取路径，根据路径打开csv按列每行到list,list每行模板， list->csv
     def commentInit(self):
-        commentFilePath = self.ui.remark_lE_path.text()
+        #commentFilePath = self.ui.remark_lE_path.text()
+        commentFilePath = './data.csv'
+        n = self.LabelClassDict.__len__()
         with open(commentFilePath, 'r') as f:
             reader = csv.reader(f)
             for i in reader:
-                new_comment = i[1][0:50]
+                #new_comment = i[1][0:50]
+                new_comment = i[1]
                 curRow = self.ui.remark_lW_list.rowCount()
                 self.ui.remark_lW_list.insertRow(curRow)
                 self.ui.remark_lW_list.setItem(curRow, 0, QTableWidgetItem(new_comment))
-                self.ui.remark_lW_list.setItem(curRow, 1, QTableWidgetItem('待标注'))
+                for k in range(n):
+                    self.ui.remark_lW_list.setItem(curRow, k+1, QTableWidgetItem('待标注'))
+                    #self.ui.remark_lW_list.setItem(curRow, k + 1, QTableWidgetItem(i[k+2]))
+                    #此行代码为导入csv文件中的“标注”
+
+    def commentSave(self):
+
+        # commentFilePath = self.ui.remark_lE_path.text()
+        commentFilePath = './data1.csv'
+        with open(commentFilePath, 'w+', newline='') as f:
+            writer = csv.writer(f)
+            for row in range(self.ui.remark_lW_list.rowCount()):
+                row_data = []
+                for column in range(self.ui.remark_lW_list.columnCount()):
+                    item = self.ui.remark_lW_list.item(row, column)
+                    #rowdata.append(unicode(item.text()).encode('utf8'))
+                    row_data.append(item.text())
+                writer.writerow(row_data)
+
+                #f.write(f'{row_data[0]},{row_data[1]}\n')
+        print("保存成功")
 
     def remarkStart(self):
         # 添加信号和槽。
         self.ui.remark_cB_class.currentIndexChanged.connect(self.comboBox_label_choose)
         self.ui.remark_pB_open.clicked.connect(self.commentInit)
+        self.ui.remark_pB_save.clicked.connect(self.commentSave)
 
     def getLabel(self, file_path):
         self.LabelClassDict = read_json(file_path)
